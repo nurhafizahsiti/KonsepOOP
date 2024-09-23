5d1fb6720c16f3e811d185ddefbcfc5da7c81be8
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-09-23 20:19:36 +0700

    Add GreeterTest.kt

--- src/main/kotlin/id/ac/polbeng/fiza/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/fiza/test_kelas/GreeterTest.kt
@@ -3,4 +3,9 @@ package id.ac.polbeng.fiza.test_kelas
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = ("Hello Programmer")
+    println(greeter.with_ret_val("Dono"))
 }
