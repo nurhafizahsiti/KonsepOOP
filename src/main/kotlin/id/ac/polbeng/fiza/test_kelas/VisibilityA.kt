774614bd455a360aa645ca0b9b5b0c4d23321d33
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-09-26 11:40:18 +0700

    Add VisibilityA.kt

---
+++ src/main/kotlin/id/ac/polbeng/fiza/test_kelas/VisibilityA.kt
@@ -0,0 +1,50 @@
+package id.ac.polbeng.fiza.test_kelas
+
+private class privateExample{
+    private val i = 1
+
+    private fun doSomething(){
+        println("Welcome to Private access!")
+        println("Private access : $i")
+    }
+    fun calldoSomething(){
+        doSomething();
+    }
+}
+//protected
+open class C(){
+    protected val i = 1
+}
+class D: C(){
+    fun getValue(): Int{
+        println("Welcome to Protected access!")
+        return i
+    }
+}
+//internal
+class internalExample {
+    internal val i = 1
+    internal fun doSomething() {
+        println("Welcome to Internal access!")
+        println("Internal access : $i")
+    }
+}
+//public
+class publicExample{
+    val i = 1
+    fun doSomething(){
+        println("Welcome to Public access!")
+        println("Public access : $i")
+    }
+}
+
+fun main() {
+    val objPrivate = privateExample()
+    objPrivate.calldoSomething()
+    val objProtected = D()
+    println(println("Protected access : ${objProtected.getValue()}"))
+    val objInternal = internalExample()
+    objInternal.doSomething()
+    val objPublic = publicExample()
+    objPublic.doSomething()
+}
